4cb7194319e32d5c00b8d0d60a2b9f7047a79dc4
Robin Gareus <robin@gareus.org>
2008-10-21 18:09:13 +0200

    english language file for config settings.

---
+++ lang/en/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * English language file for config of the oauth plugin
+ *
+ * @author    Robin Gareus <robin@gareus.org
+ */
+$lang['manager_admin']     = 'allow managers to administrate ALL oauth-tokens';
+$lang['custom_base_url']   = 'set custom end-point base-url (leave blank for default/autodetect)';
+
+//Setup VIM: ex: et ts=2 enc=utf-8 :
